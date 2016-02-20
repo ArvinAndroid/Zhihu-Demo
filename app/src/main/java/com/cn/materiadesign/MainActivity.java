6b3d1b5767f9ef4e8e1b18c7109e66a4aefb18bc
chenjun <cccccc.jun@gmail.com>
2016-02-20 19:08:25 +0800

    add DrawerLayout and NavigationView

--- app/src/main/java/com/cn/materiadesign/MainActivity.java
+++ app/src/main/java/com/cn/materiadesign/MainActivity.java
@@ -1,6 +1,8 @@
 package com.cn.materiadesign;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -8,10 +10,16 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        navView = (NavigationView) findViewById(R.id.nav_view);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
