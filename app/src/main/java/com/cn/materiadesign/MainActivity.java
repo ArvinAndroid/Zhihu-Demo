d15a2c55cf5e772bab4fb8b2eec68876a5043b61
chenjun <cccccc.jun@gmail.com>
2016-02-25 00:51:28 +0800

    bind Tab ViewPager

--- app/src/main/java/com/cn/materiadesign/MainActivity.java
+++ app/src/main/java/com/cn/materiadesign/MainActivity.java
@@ -4,10 +4,21 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+
+import com.cn.materiadesign.fragment.Tab1Fragment;
+import com.cn.materiadesign.fragment.Tab2Fragment;
+import com.cn.materiadesign.fragment.Tab3Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,10 +42,45 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pageer);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment());
+        adapter.addFragment(new Tab2Fragment());
+        adapter.addFragment(new Tab3Fragment());
+        viewPager.setAdapter(adapter);
+
         TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
-        tabs.setTabTextColors(Color.WHITE,Color.RED);
-        tabs.addTab(tabs.newTab().setText("Tab 1"));
-        tabs.addTab(tabs.newTab().setText("Tab 2"));
-        tabs.addTab(tabs.newTab().setText("Tab 3"));
+        tabs.setTabTextColors(Color.WHITE, Color.RED);
+        tabs.setupWithViewPager(viewPager);
     }
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        List<Fragment> fragments = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Tab" + position + 1;
+        }
+
+        public void addFragment(Fragment fragment) {
+            fragments.add(fragment);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/cn/materiadesign/fragment/Tab1Fragment.java
@@ -0,0 +1,22 @@
+package com.cn.materiadesign.fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cn.materiadesign.R;
+
+/**
+ * Created by jun on 2/24/16.
+ */
+public class Tab1Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab1, container, false);
+    }
+}
