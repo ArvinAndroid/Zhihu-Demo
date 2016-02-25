58b3f65be76c2a6505e2b08604144e1cf6a80eea
chenjun <cccccc.jun@gmail.com>
2016-02-25 23:58:38 +0800

    add RecyclerView,CardView,Darwer Menu Image and head img

--- app/src/main/java/com/cn/materiadesign/fragment/Tab1Fragment.java
+++ app/src/main/java/com/cn/materiadesign/fragment/Tab1Fragment.java
@@ -1,8 +1,10 @@
 package com.cn.materiadesign.fragment;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +16,42 @@ import com.cn.materiadesign.R;
  */
 public class Tab1Fragment extends Fragment {
 
+    private RecyclerView recyclerView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab1, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler);
+        recyclerView.setAdapter(new ContentAdapter());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
+
+    class ContentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 10;
+        }
+
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.item_list, parent, false));
+        }
     }
 }
